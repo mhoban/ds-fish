e9b8c6542a1b538e9adb822463053b05a1a926bf
mykle hoban <mhoban@hawaii.edu>
2018-05-01 19:02:38 -1000

    code updates

--- fishgraphs.R
+++ fishgraphs.R
@@ -6,80 +6,40 @@ if (!exists('loaded')) {
 
 
 # load these things
-library(ggsci)
 library(tidyverse)
-library(ggplot2)
+#library(ggplot2)
 library(scales)
+library(ggsci)
 
 # set working directory
-setwd("~/projects/deep-sea fisheries/")
+setwd("~/projects/deep-sea fisheries/200-400/data/")
 
 # load and filter species table
-my.species <- read.csv("200-400/data/species.csv") %>% filter(owner=='mykle') %>% droplevels()
+my.species <- read.csv("species.csv") %>% filter(owner=='mykle') %>% droplevels()
 
 
-####### FAO DATA #######
-# read and format FAO data
-fao.data <- read.csv("200-400/data/fao-data.csv") %>%
-  # convert from wide to long format
-  gather("year","catch",-c(1:5)) %>% 
-  # R puts an X before column names that are numbers, the next three lines
-  # make sure that years are treated as years in numeric format
-  separate(year,into=c("nothing","year"),"(?<=X)(?=[0-9])") %>%
-  select(-nothing) %>%
-  mutate(year=as.numeric(year))
+##### LOAD DATA ########
+all.data <- read_csv("200-400/data/fao-sau-combined.csv",guess_max = 100000) %>%
+  filter(scientific_name %in% my.species$scientific.name) %>%
+  droplevels()
 
-# subset FAO data to species list
-my.fao <- fao.data %>%
-  filter(common.name %in% my.species$common.name) %>%
-  # this adds in the missing data from the species table
-  left_join(.,my.species,by="common.name") %>%
-  droplevels() %>% 
-  select(-owner) %>%
-  mutate(common.name=factor(common.name))
-  
-
-####### SAU DATA #######
-sau.data <- read.csv("200-400/data/sau-data.csv")
-my.sau <- sau.data %>% 
-  filter(common.name %in% my.species$common.name) %>%
-  rename(country=fishing.entity) %>%
-  droplevels() 
-  
-
-
-# sum total catch per species by year and country 
-fao <- my.fao %>% 
-  group_by(year,common.name,country) %>% 
-  summarise(total.catch=sum(catch)) %>% 
-  ungroup() %>% 
-  arrange(year,common.name) %>% 
-  mutate_if(is.factor,as.character)
-sau <- my.sau %>% 
-  group_by(year,common.name,country) %>% 
-  summarise(total.catch=sum(tonnes)) %>% 
+# aggretate and summarize dataset
+combined <- all.data %>%
+  group_by(year,common_name,country,source) %>%
+  summarise(total.catch=sum(catch)) %>%
   ungroup() %>%
-  arrange(year,common.name) %>% 
-  mutate_if(is.factor,as.character)
-
-# combine both catch datasets into one distinguished by a factor denoting its
-# source (i.e. fao/sau) and rebuild all the factors at the end
-combined <- full_join(fao,sau,by=c('year','common.name','country'),suffix=c('.fao','.sau')) %>% 
-  mutate_all(funs(replace(., which(is.na(.)), 0))) %>%    # replace NAs with zeroes
-  gather("source","catch",-c(1:3)) %>%                    # make into long format
-  mutate_if(is.character,as.factor) %>%                   # rebuild factors from character vectors
-  arrange(year,common.name,source)                        # sort the dataset by year, species, and data source)
-
+  arrange(year,common_name) %>%
+  mutate_if(is.character,as.factor)
 
 if (!exists('dontplot') | dontplot == F)
 {
   ## plot stacked bar graphs of country by year for each species
   # create a labeller function so to display source categories better
-  source.label = as_labeller(c('total.catch.fao' = 'Total Catch (FAO)', 'total.catch.sau' = 'Total Catch (SAU)'))
+  source.label = as_labeller(c('fao' = 'Total Catch (FAO)', 'sau' = 'Total Catch (SAU+FAO)'))
   # plot catch by country for each species. in RStudio you can cycle through the different plots
   # country.plotz will be a list of each plot object
-  country.plotz <- map(levels(combined$common.name),function(taxon) {
-    plotr <- ggplot(combined %>% filter(common.name == taxon), aes(x=year,y=catch,fill=country)) +
+  country.plotz <- map(levels(combined$common_name),function(taxon) {
+    plotr <- ggplot(combined %>% filter(common_name == taxon), aes(x=year,y=total.catch,fill=country)) +
       geom_col(color="black") +
       theme_minimal() +
       scale_y_continuous(name="Total Catch",labels=comma) + 
@@ -93,22 +53,27 @@ if (!exists('dontplot') | dontplot == F)
   })
   
   # uncomment to create a multi-page PDF with one plot on each page
-  pdf("200-400/plots/country-year.pdf",paper='USr')
-  walk(country.plotz,function(plotr) {
-    print(plotr)
-  })
-  dev.off()
+  # pdf("200-400/plots/country-year.pdf",paper='USr')
+  # walk(country.plotz,function(plotr) {
+  #   print(plotr)
+  # })
+  # dev.off()
   
   # plot step graphs per species
   # as above, step.plotz will be a list of each plot object
-  global.catch <- combined %>% group_by(year,common.name,source) %>% summarise(total.catch=sum(catch)) %>% ungroup()
-  step.plotz <- map(levels(global.catch$common.name),function(taxon) {
-    plotr <- ggplot(global.catch %>% filter(common.name == taxon), aes(x=year,y=total.catch,color=source)) +
+  
+  # summarize global catch totals per year (for all countries)
+  global.catch <- combined %>% 
+    group_by(year,common_name,source) %>% 
+    summarise(gross.catch=sum(total.catch)) %>% 
+    ungroup()
+  step.plotz <- map(levels(global.catch$common_name),function(taxon) {
+    plotr <- ggplot(global.catch %>% filter(common_name == taxon), aes(x=year,y=gross.catch,color=source)) +
       geom_step() +
       theme_classic() +
       #theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-      scale_color_igv(name='Data Source',breaks=c('total.catch.fao','total.catch.sau'),labels=c('FAO','FAO+SAUP')) +
-      scale_x_continuous(name="Year",breaks=pretty(combined$year,n=10)) +
+      scale_color_igv(name='Data Source',breaks=c('fao','sau'),labels=c('FAO','FAO+SAU')) +
+      scale_x_continuous(name="Year",breaks=pretty(global.catch$year,n=10)) +
       scale_y_continuous(name="Total Catch",labels=comma) +
       ggtitle(taxon)
     cat(taxon,"\n")
@@ -117,9 +82,9 @@ if (!exists('dontplot') | dontplot == F)
   })
   
   # uncomment to create a multi-page PDF with one plot on each page
-  pdf("200-400/plots/species-total.pdf",paper='USr')
-  walk(step.plotz,function(plotr) {
-    print(plotr)
-  })
-  dev.off()
+  # pdf("200-400/plots/species-total.pdf",paper='USr')
+  # walk(step.plotz,function(plotr) {
+  #   print(plotr)
+  # })
+  # dev.off()
 }
--- sync-fao-sau.R
+++ sync-fao-sau.R
@@ -1,47 +1,125 @@
 rm(list=ls())
 library(tidyverse)
+library(stringr)
 
 setwd("~/projects/deep-sea fisheries/200-400/")
 #species <- readxl::read_excel("data/200-400 m data and paper/DeepSpeciesList_2.xlsx",sheet = "SpeciesList")
 species <- read_csv("data/species.csv")
-fao <- readr::read_csv("data/tsd_global_production_long.csv.gz",guess_max = 100000) %>% 
-  mutate_if(is.character,as.factor) %>%
+countries <- read_csv("data/ref_country.csv")
+
+
+fao <- readr::read_csv("data/merge/tsd_global_production_long.csv.gz",guess_max = 100000) %>% 
   mutate(source="fao") %>%
-  mutate(reporting_status="Reported") %>%
-  rename(common_name=species) %>%
-  select(country,year,common_name,scientific_name,reporting_status,catch,source) %>%
-  filter(scientific_name %in% species$scientific.name) %>%
+  #mutate(reporting_status="Reported") %>%
+  #rename(common_name=species) %>%
+  select(country,country_iso,country_un,fishing_area,year,common_name,scientific_name,reporting_status,catch,source) %>%
+  #filter(scientific_name %in% species$scientific.name) %>% keep the whole business
+  mutate_if(is.character,as.factor) %>%
+  arrange(year,country,common_name) %>%
   droplevels()
 
-#sau.n <- readr::read_csv("data/sau/sau-data-fixed.csv", guess_max=100000)
-sau <- readr::read_csv("data/sau/sau-data-fixed.csv", guess_max=100000) %>% 
-  mutate_if(is.character,as.factor) %>%
+
+## make sure to sum "unreported" by each sub type of unreported
+
+sau <- read_csv("data/merge/Mykle_eez_v47.csv", guess_max=100000) %>%
   mutate(source="sau") %>%
   rename(country=fishing_entity) %>%
   rename(catch=tonnes) %>%
-  select(country,year,common_name,scientific_name,reporting_status,catch,source) %>%
-  filter(scientific_name %in% species$scientific.name) %>% 
+  #filter(scientific_name %in% species$scientific.name) %>% no filter species
+  left_join(countries,by=c('country'='name_en')) %>%
+  rename(country_un=un_code) %>%
+  rename(country_iso=iso_3_code) %>%
+  select(country,country_iso,country_un,fao_area,year,common_name,scientific_name,reporting_status,catch,source) %>%
+  rename(fishing_area=fao_area) %>%
+  mutate_if(is.character,as.factor) %>%
+  arrange(year,country,common_name) %>%
   droplevels()
 
-sumz <- sau %>% group_by(country,year,scientific_name,reporting_status) %>% summarise(total_catch=sum(catch))
-sumzer <- sumz %>% group_by(scientific_name,year) %>% summarise(total_catch=sum(total_catch))
+highseas <- read_csv("data/merge/Les_eez_hs_04-10-2018_v47.csv",guess_max=100000) %>%
+  mutate(source="sau") %>%
+  #mutate(common_name="Argentines") %>%
+  rename(country=fishing_entity) %>%
+  rename(catch=tonnes) %>%
+  #filter(scientific_name %in% species$scientific.name) %>%
+  left_join(countries,by=c('country'='name_en')) %>%
+  left_join(species,by=c('scientific_name'='scientific.name')) %>%
+  rename(common_name=common.name) %>%
+  rename(country_un=un_code) %>%
+  rename(country_iso=iso_3_code) %>%
+  select(country,country_iso,country_un,fao_area,year,common_name,scientific_name,reporting_status,catch,source) %>%
+  rename(fishing_area=fao_area) %>%
+  mutate_if(is.character,as.factor) %>%
+  arrange(year,country,common_name) %>%
+  droplevels()
 
-all_data <- rbind(fao,sau)
+argentines <- read_csv("data/merge/Les_GenusArgentina_v47.csv",guess_max=100000) %>%
+  mutate(source="sau") %>%
+  mutate(common_name="Argentine") %>%
+  rename(country=fishing_entity) %>%
+  rename(catch=tonnes) %>%
+  #filter(scientific_name %in% species$scientific.name) %>%
+  left_join(countries,by=c('country'='name_en')) %>%
+  #left_join(species,by=c('scientific_name'='scientific.name')) %>%
+  #rename(common_name=common.name) %>%
+  rename(country_un=un_code) %>%
+  rename(country_iso=iso_3_code) %>%
+  select(country,country_iso,country_un,fao_area,year,common_name,scientific_name,reporting_status,catch,source) %>%
+  rename(fishing_area=fao_area) %>%
+  mutate_if(is.character,as.factor) %>%
+  arrange(year,country,common_name) %>%
+  droplevels()
 
-readr::write_excel_csv(all_data,"data/fao-sau-combined.csv",col_names = T)
-f <- readr::read_csv("data/fao-sau-combined.csv")
+
+#sau.n <- readr::read_csv("data/sau/sau-data-fixed.csv", guess_max=100000)
+# sau <- readr::read_csv("data/sau/sau-data-fixed.csv.gz", guess_max=100000) %>% 
+#   mutate(source="sau") %>%
+#   #mutate(country_code=NA) %>%
+#   rename(country=fishing_entity) %>%
+#   rename(catch=tonnes) %>%
+#   filter(scientific_name %in% species$scientific.name) %>% 
+#   left_join(countries,by=c('country'='name_en')) %>%
+#   rename(country_un=un_code) %>%
+#   rename(country_iso=iso_3_code) %>%
+#   select(country,country_iso,country_un,area_name,year,common_name,scientific_name,reporting_status,catch,source) %>%
+#   rename(fishing_area=area_name) %>%
+#   mutate_if(is.character,as.factor) %>%
+#   arrange(year,country,common_name) %>%
+#   droplevels()
+
+all_data <- bind_rows(fao,sau,highseas,argentines) %>%
+  mutate_if(is.character,as.factor) %>%
+  arrange(year,country,common_name)
+
+#all_data_filtered <- all_data[!duplicated(all_data[,c('scientific_name','year','reporting_status','catch','fishing_area','country')]),]
+all_data_filtered <- all_data %>% filter(!duplicated(all_data))
+
+write_excel_csv(all_data,"data/fao-sau-combined.csv",col_names = T)
+
+fao <- all_data %>% filter(source == 'fao') %>% droplevels()
+sau <- all_data %>% filter(source == 'sau') %>% droplevels()
+
+
+
+fao.areas <- levels(fao$fishing_area)
+sau.areas <- levels(sau$fishing_area)
+fao.only.areas <- fao.areas[!(fao.areas %in% sau.areas)]
+sau.only.areas <- sau.areas[!(sau.areas %in% fao.areas)]
 
 fao.countries <- levels(fao$country)
 sau.countries <- levels(sau$country)
+#sau.countries <- levels(factor(fixed_countries))
 sau.only.countries <- sau.countries[!(sau.countries %in% fao.countries)]
+fao.only.countries <- fao.countries[!(fao.countries %in% sau.countries)]
 
 fao.species <- levels(fao$scientific_name)
 sau.species <- levels(sau$scientific_name)
 sau.only.species <- sau.species[!(sau.species %in% fao.species)]
+fao.only.species <- fao.species[!(fao.species %in% sau.species)]
 
 fao.common <- levels(fao$common_name)
 sau.common <- levels(sau$common_name)
 sau.only.common <- sau.common[!(sau.common %in% fao.common)]
+fao.only.common <- fao.common[!(fao.common %in% sau.common)]
 # 
 # View(sau.only.species)
 # View(fao.species)
