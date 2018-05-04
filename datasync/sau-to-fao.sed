2423252630435e5f4f2e73dd68b37e197484b8ed
mykle hoban <mhoban@hawaii.edu>
2018-05-03 16:00:37 -1000

    Added sync data files and updated sync script

---
+++ datasync/sau-to-fao.sed
@@ -0,0 +1,30 @@
+## countries
+s/Azores Isl. (Portugal)/Portugal/g
+s/Channel Isl. (UK)/Channel Islands/g
+s/Congo (ex-Zaire)/Congo/g
+s/Congo, R. of/Congo/g
+s/Faeroe Isl. (Denmark)/Faroe Islands/g
+s/Falkland Isl. (UK)/Falkland Is.(Malvinas)/g
+s/Korea (South)/"Korea, Republic of"/g
+s/Madeira Isl. (Portugal)/Portugal/g
+s/Mayotte (France)/Mayotte/g
+s/Norfolk Isl. (Australia)/Norfolk Island/g
+s/North Cyprus/Cyprus/g
+s!Saint Pierre & Miquelon (France)!St. Pierre and Miquelon!g
+s!Saint Vincent & the Grenadines!Saint Vincent/Grenadines!g
+s/South Cyprus/Cyprus/g
+s/Taiwan/Taiwan Province of China/g
+s/USA/United States of America/g
+
+## species
+#s/Emmelichthys nitidus nitidus/Emmelichthys nitidus/g
+#s/Merluccius gayi gayi/Merluccius gayi/g
+#s/Angler/Angler(=Monk)/g
+#s/Black cuskeel/Black cusk-eel/g
+#s/European argentine/Argentine/g
+#s/Blackspot seabream/Blackspot(=red) seabream/g
+#s/Eatons skate/"Eaton's skate"/g
+#s/Hapuka/Hapuku wreckfish/g
+
+### species tweaks (mostly changing genus-level taxa to a format understood by FAO)
+#s/,Argentina,/,Argentina spp,/g
--- sync-fao-sau.R
+++ sync-fao-sau.R
@@ -1,20 +1,18 @@
 rm(list=ls())
 library(tidyverse)
-library(stringr)
-
 
 ### Sync SAU and FAO data
 ## Steps:
 ## 1. run sau-to-fao.sed on SAU .csv files (this synchronizes country names)
-## $ sed -i'.backup' -fsau-to-fao.sed 'Mykle_v2_v47.csv'
+## $ sed -i'.original' -f sau-to-fao.sed 'Mykle_v2_v47.csv'
 ## 2. run this script
 
-setwd("~/projects/deep-sea fisheries/200-400/")
-species_translate <- read_csv("data/merge/realmerge/species-translate.csv")
-countries <- read_csv("data/ref_country.csv")
+setwd("~/projects/deep-sea fisheries/200-400/code/")
+species_translate <- read_csv("datasync/species-translate.csv.gz")
+countries <- read_csv("datasync/ref_country.csv.gz")
 
 ## Read entire FAO database (every entry for production type 'capture')
-fao <- read_csv("data/merge/realmerge/tsd_global_production_long.csv.gz",guess_max = 100000) %>% 
+fao <- read_csv("datasync/tsd_global_production_long.csv.gz",guess_max = 100000) %>% 
   filter(scientific_name %in% species_translate$scientific_name_fao) %>%
   mutate(source="fao") %>% # designate source as FAO
   rename(fao_area=fishing_area) %>%
@@ -24,7 +22,7 @@ fao <- read_csv("data/merge/realmerge/tsd_global_production_long.csv.gz",guess_m
 ## Read, calculate, and format SAU dataset
 
 # this file contains all the latest SAU data for our class species list
-sau <- read_csv("data/merge/realmerge/Mykle_v2_v47.csv") %>%
+sau <- read_csv("datasync/Mykle_v2_v47.csv.gz") %>%
   left_join(species_translate,by=c("scientific_name"="scientific_name_sau")) %>%
   left_join(countries,by=c("fishing_entity"="name_en")) %>%
   mutate(source="sau") %>%
@@ -44,4 +42,4 @@ sau <- read_csv("data/merge/realmerge/Mykle_v2_v47.csv") %>%
 all_data <- union(fao,sau) %>%
   arrange(source,year,country,common_name,reporting_status)
 
-write_excel_csv(all_data,"data/merge/realmerge/fao-sau-combined.csv",col_names = T)
+write_excel_csv(all_data,"datasync/fao-sau-combined.csv",col_names = T)
